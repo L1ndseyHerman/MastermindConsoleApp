cacbece4f83c62557289dffe899ecd55c5c1cb97
L1ndseyHerman <lherman21@jcu.edu>
2026-07-02 13:41:34 -0400

    Better input error handling

--- Program.cs
+++ Program.cs
@@ -4,21 +4,38 @@ Random rand = new();
 
 List<string> numberCodeAsStrings = [];
 
-for (int i = 0; i < 4; i++)
+for (int index = 0; index < 4; index++)
 {
     numberCodeAsStrings.Add(rand.Next(1, 7).ToString());
 }
 
 Console.WriteLine(numberCodeAsStrings[0] + numberCodeAsStrings[1] + numberCodeAsStrings[2] + numberCodeAsStrings[3]);
 
-string? guess1 = Console.ReadLine();
+string? firstGuessString = Console.ReadLine();
 
-// Placeholder for if the user types something other than 1-6:
-if (guess1 != "a")
+if (firstGuessString == null || firstGuessString.Length != 4)
 {
     Console.WriteLine("Some error message");
 }
 else
 {
-    Console.WriteLine("Placeholder 2");
+    bool hasNonValidInput = false;
+
+    for (int index = 0; index < 4; index++)
+    {
+        if (!(firstGuessString[index] == '1' || firstGuessString[index] == '2' || firstGuessString[index] == '3' || firstGuessString[index] == '4' || firstGuessString[index] == '5' || firstGuessString[index] == '6'))
+        {
+            hasNonValidInput = true;
+        }
+    }
+
+    if (hasNonValidInput)
+    {
+        Console.WriteLine("Some error message");
+    }
+    else
+    {
+        Console.WriteLine("Placeholder 2");
+    }
+
 }

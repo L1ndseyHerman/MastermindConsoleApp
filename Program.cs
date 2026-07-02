c5f8f9ed668256ee20b7a98e1c6cc5ddd6e93c69
L1ndseyHerman <lherman21@jcu.edu>
2026-07-02 13:22:06 -0400

    The 4-digit code

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
 ﻿Console.WriteLine("Placeholder");
 
-string? guess = Console.ReadLine();
+Random rand = new();
+
+List<string> numberCodeAsStrings = [];
+
+for (int i = 0; i < 4; i++)
+{
+    numberCodeAsStrings.Add(rand.Next(1, 7).ToString());
+}
+
+Console.WriteLine(numberCodeAsStrings[0] + numberCodeAsStrings[1] + numberCodeAsStrings[2] + numberCodeAsStrings[3]);
+
+string? guess1 = Console.ReadLine();
 
 // Placeholder for if the user types something other than 1-6:
-if (guess != "a")
+if (guess1 != "a")
 {
     Console.WriteLine("Some error message");
 }

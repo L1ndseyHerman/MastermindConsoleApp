00f9f72e1af1cc751503a7d9a84ffa14b93a2286
L1ndseyHerman <lherman21@jcu.edu>
2026-07-03 09:58:23 -0400

    I think that's everything to test for HandleGuess() unless I'm forgetting something....

--- MastermindConsoleAppTests/ProgramTests.cs
+++ MastermindConsoleAppTests/ProgramTests.cs
@@ -85,7 +85,151 @@ namespace MastermindConsoleAppTests
         [Fact]
         public void HandleGuess_WherePlayerGuessesAMixOfRightSpotWrongSpotAndWrongNumbers_ReturnsTheMix()
         {
-            string guessResults = Program.HandleGuess("1234", "1356");
+            string guessResults = Program.HandleGuess("1234", "1366");
+
+            Assert.Equal("+-", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WherePlayerGuessesMoreThanOneOfTheSameNumberButThereIsOnlyOneInTheCode_OnlyReturnsOneResultForTheNumber()
+        {
+            string guessResults = Program.HandleGuess("1234", "1111");
+
+            Assert.Equal("+", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WherePlayerGuessesMoreThanOneOfTheSameNumberButThereIsOnlyOneInTheCode_OnlyReturnsOneResultForTheNumberDifGuess()
+        {
+            string guessResults = Program.HandleGuess("1234", "3333");
+
+            Assert.Equal("+", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WherePlayerGuessesMoreThanOneOfTheSameNumberButThereIsOnlyOneInTheCode_OnlyReturnsOneResultForTheNumberMinusesToo()
+        {
+            string guessResults = Program.HandleGuess("1234", "6611");
+
+            Assert.Equal("-", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WherePlayerGuessesMoreThanOneOfTheSameNumberButThereIsOnlyOneInTheCode_OnlyReturnsOneResultForTheNumberMinusesTooDifGuess()
+        {
+            string guessResults = Program.HandleGuess("1234", "3366");
+
+            Assert.Equal("-", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WherePlayerGuessesMoreThanOneOfTheSameNumberButThereIsOnlyOneInTheCode_OnlyReturnsOneResultForTheNumberPlusAndMinus()
+        {
+            string guessResults = Program.HandleGuess("1234", "1313");
+
+            Assert.Equal("+-", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WherePlayerGuessesMoreThanOneOfTheSameNumberButThereIsOnlyOneInTheCode_OnlyReturnsOneResultForTheNumberPlusAndMinusDifGuess()
+        {
+            string guessResults = Program.HandleGuess("1234", "3131");
+
+            Assert.Equal("+-", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WhereCodeHasAllTheSameNumber_CanReturnPlusses()
+        {
+            string guessResults = Program.HandleGuess("1111", "1111");
+
+            Assert.Equal("++++", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WhereCodeHasAllTheSameNumber_CanReturnTheEmptyString()
+        {
+            string guessResults = Program.HandleGuess("1111", "6666");
+
+            Assert.Equal("", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WhereCodeHasAllTheSameNumber_CanReturnMix()
+        {
+            string guessResults = Program.HandleGuess("1111", "1616");
+
+            Assert.Equal("++", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WhereCodeHasMultipleSameNumbers_CanReturnAllPlusses()
+        {
+            string guessResults = Program.HandleGuess("1212", "1212");
+
+            Assert.Equal("++++", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WhereCodeHasMultipleSameNumbers_CanReturnSomePlusses()
+        {
+            string guessResults = Program.HandleGuess("1212", "1266");
+
+            Assert.Equal("++", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WhereCodeHasMultipleSameNumbers_CanReturnSomePlussesDifGuess()
+        {
+            string guessResults = Program.HandleGuess("1212", "6612");
+
+            Assert.Equal("++", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WhereCodeHasMultipleSameNumbers_CanReturnAllMinuses()
+        {
+            string guessResults = Program.HandleGuess("1212", "2121");
+
+            Assert.Equal("----", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WhereCodeHasMultipleSameNumbers_CanReturnSomeMinuses()
+        {
+            string guessResults = Program.HandleGuess("1212", "2166");
+
+            Assert.Equal("--", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WhereCodeHasMultipleSameNumbers_CanReturnSomeMinusesDifGuess()
+        {
+            string guessResults = Program.HandleGuess("1212", "6621");
+
+            Assert.Equal("--", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WhereCodeHasMultipleSameNumbers_CanReturnTheEmptyString()
+        {
+            string guessResults = Program.HandleGuess("1212", "6666");
+
+            Assert.Equal("", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WhereCodeHasMultipleSameNumbers_CanReturnMixOfPlusAndMinus()
+        {
+            string guessResults = Program.HandleGuess("1212", "1221");
+
+            Assert.Equal("++--", guessResults);
+        }
+
+        [Fact]
+        public void HandleGuess_WhereCodeHasMultipleSameNumbers_CanReturnMixOfPlusAndMinusDifGuess()
+        {
+            string guessResults = Program.HandleGuess("1212", "2616");
 
             Assert.Equal("+-", guessResults);
         }

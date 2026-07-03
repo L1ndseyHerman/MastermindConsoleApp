d5f892beb83a4fdcde319ed1628291083a798bad
L1ndseyHerman <lherman21@jcu.edu>
2026-07-03 07:41:28 -0400

    Done with tests for CheckIfHasNonValidInput() :)

--- MastermindConsoleAppTests/ProgramTests.cs
+++ MastermindConsoleAppTests/ProgramTests.cs
@@ -5,12 +5,33 @@ namespace MastermindConsoleAppTests
 {
     public class ProgramTests
     {
-        [Fact]
-        public void CheckIfHasNonValidInput_WithNonValidInput_ReturnsTrue()
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("123")]
+        [InlineData("12345")]
+        [InlineData("abcd")]
+        [InlineData("?!*/")]
+        [InlineData("7890")]
+        [InlineData("123a")]
+        [InlineData("7123")]
+        public void CheckIfHasNonValidInput_WithNonValidInput_ReturnsTrue(string guessString)
         {
-            bool result = Program.CheckIfHasNonValidInput("a");
+            bool result = Program.CheckIfHasNonValidInput(guessString);
 
             Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("1234")]
+        [InlineData("6543")]
+        [InlineData("1212")]
+        [InlineData("1111")]
+        public void CheckIfHasNonValidInput_WithValidInput_ReturnsFalse(string guessString)
+        {
+            bool result = Program.CheckIfHasNonValidInput(guessString);
+
+            Assert.False(result);
         }
     }
 }
